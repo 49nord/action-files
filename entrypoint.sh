5ba7e9a717687bc9af3afb870d52e35c4ed81f1f
Marc Brinkmann <marc@49nord.de>
2021-02-28 21:15:53 +0100

    Properly prefix input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ set -eu
 
 # We use SSH agent to avoid having to store a copy of the private key on disk.
 eval $(ssh-agent)
-echo ${SSH_KEY} | ssh-add -
+echo ${INPUT_SSH_KEY} | ssh-add -
 
-if [ ! -z "${SSH_ARGS}" ]; then
-  RSYNC_SSH_ARGS="-e \"${SSH_ARGS}\""
+if [ ! -z "${INPUT_SSH_ARGS}" ]; then
+  RSYNC_SSH_ARGS="-e \"${INPUT_SSH_ARGS}\""
 else
   RSYNC_SSH_ARGS=""
 fi
 
 set -x
 
-exec rsync ${RSYNC_SSH_ARGS} ${RSYNC_ARGS} "${SRC}" "${DEST}"
+exec rsync ${RSYNC_SSH_ARGS} ${INPUT_RSYNC_ARGS} "${INPUT_SRC}" "${INPUT_DEST}"
