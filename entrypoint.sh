61989496eaf65167b9758215cc42f0d3429c8645
Marc Brinkmann <git@marcbrinkmann.de>
2021-03-05 13:43:45 +0100

    Fix typo in parameter name

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ KNOWN_HOSTS=$(mktemp)
 echo "${INPUT_KNOWN_HOSTS}" > ${KNOWN_HOSTS}
 
 # Determine the target host and directory, in case we need to create id
-TARGET_HOST=${dest%:*}
-TARGET_DIR=${dest#:*}
+TARGET_HOST=${INPUT_DEST%:*}
+TARGET_DIR=${INPUT_DEST#:*}
 
 # Setup the SSH command.
 SSH="ssh -o UserKnownHostsFile=${KNOWN_HOSTS} ${INPUT_SSH_ARGS}"
